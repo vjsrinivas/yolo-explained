0d6d8794812c249f6c8cfe5711c6478676d9fb0a
Vijay Rajagopal <vijaythinks@gmail.com>
2023-06-16 22:06:19 -0400

    try cdn method

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/vjsrinivas/yolo-explained/tree/gh-pages' : '',
   output: 'export',
   images: {
     unoptimized: true
